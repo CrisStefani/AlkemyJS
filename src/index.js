8120ba42fa990cabcb48110df09418c8ba0f2900
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-09-28 14:56:24 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/index.scss';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+import storage from './localStorage/storage';
+import rootReducer from './reducers/index';
 
-import Routes from './routes/Routes';
+const persistState = storage.loadState();
+
+const store = createStore(rootReducer, persistState);
+
+store.subscribe(() => {
+  storage.saveState(store.getState());
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
+export default store;
