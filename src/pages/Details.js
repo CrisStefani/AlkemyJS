b120f48d9e54bb5a79983f19455ad753e99a1fe8
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-09-29 18:52:24 -0300

    detail hero

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -14,52 +14,31 @@ const Details = props => {
 
     if (option === 'appearance') {
       const { appearance } = currentHero;
-      const { gender } = appearance;
-      return (
-        <div>
-          <h3>Gender</h3>
-          <p>{gender}</p>
-          <h3>Eye Color</h3>
-          <p>{appearance['eye-color']}</p>
-          <h3>Hair Color</h3>
-          <p>{appearance['hair-color']}</p>
-          <h3>Height</h3>
-          <p>{`${appearance.height[0]} - ${appearance.height[1]}`}</p>
-          <h3>Race</h3>
-          <p>{appearance.race}</p>
-          <h3>weight</h3>
-          <p>{`${appearance.weight[0]} - ${appearance.weight[1]}`}</p>
-        </div>
-      );
-    }
-    if (option === 'biography') {
+      
       const { biography } = currentHero;
       return (
         <div>
-          <h3>Aliases</h3>
+          <h3>Peso</h3>
+          <p>{`${appearance.weight[0]} - ${appearance.weight[1]}`}</p>
+          <h3>Altura</h3>
+          <p>{`${appearance.height[0]} - ${appearance.height[1]}`}</p>
+          <h3>Nombre</h3>
+          {' '}
+          <p>{biography['full-name']}</p>
+          <h3>Alias</h3>
           {biography.aliases.map(alias => (
             <p key={alias}>{alias}</p>
           ))}
-          <h3>Alignment</h3>
-          {' '}
-          <p>{biography.alignment}</p>
-          <h3>Alter-egos</h3>
-          {' '}
-          <p>{biography['alter-egos']}</p>
-          <h3>First Appearance</h3>
-          <p>{biography['first-appearance']}</p>
-          <h3>Full-name</h3>
-          {' '}
-          <p>{biography['full-name']}</p>
-          <h3>Place Of Birth</h3>
-          {' '}
-          <p>{biography['place-of-birth']}</p>
-          <h3>Publisher</h3>
-          {' '}
-          <p>{biography.publisher}</p>
+          <h3>Color de Ojos</h3>
+          <p>{appearance['eye-color']}</p>
+          <h3>Color de cabello</h3>
+          <p>{appearance['hair-color']}</p>
+          
+                    
         </div>
       );
     }
+    
     if (option === 'powerstats') {
       const { powerstats } = currentHero;
       return (
