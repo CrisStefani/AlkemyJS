82c3cbe00fac7c0588dcd03e75c188e936715ba1
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-09-29 18:26:09 -0300

    formik

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,81 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 function Login(){
+    const [formSend, setFormSubmit] = useState(false);
     return(
-        <div className="container">
-            <form>
-                <div className="mb-3">
+        <Formik 
+        initialValues={{
+            email:"",
+            password:''
+        }}
+        validate={(val)=>{
+            let errors = {};
+
+                if(!val.email){
+                    errors.email = 'Por favor ingresa un correo'
+                } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(val.email)){
+                    errors.email = 'El email solo puede contener letras, numeros, puntos, guiones o guion bajo'
+                }
+                if(!val.password){
+                    errors.password = 'Por favor ingresa una contraseña'
+                } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(val.password)){
+                    errors.password = 'La contraseña debe tener letras solamente'
+                }
+
+                return errors;
+        }}
+        onSubmit={(val, {resetForm})=>{
+            resetForm();
+            console.log('Formulario enviado');
+            setFormSubmit(true);
+            setTimeout(() => setFormSubmit(false), 5000);
+
+        }}
+        >
+            {( {errors} ) => (
+                <div className="contenedor">
+                <div className="formulario">
+            <Form>
+                <div>
                     <label for="exampleInputEmail1" className="form-label">Dirección de mail</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
-                    <div id="emailHelp" className="form-text">No compartir los datos</div>
+                    <Field 
+                    type="email" 
+                    className="form-control" 
+                    id="exampleInputEmail1" 
+                    name='email' 
+                    aria-describedby="emailHelp" 
+                    placeholder='email@email.com'
+                     />                   
+                    
                 </div>
-                <div className="mb-3">
+                <ErrorMessage name='email' component={()=> (
+                         <div className="error">{errors.email}</div>
+                     )} />
+                <div>
                     <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" required/>
+                    <Field
+                    type="password" 
+                    className="form-control" 
+                    id="exampleInputPassword1" 
+                    name='password' 
+                    
+                      
+                    />
+                    <ErrorMessage name='email' component={()=> (
+                         <div className="error">{errors.password}</div>
+                     )} />
                 </div>
   
-            <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
+            <button type="submit" >Submit</button>
+            {formSend && <p className="exito">Formulario enviado con exito!</p>}
+            </Form>
+        </div>    
+        </div>    
+            ) }
 
+        </Formik>
+
+        
     );
 }
 
