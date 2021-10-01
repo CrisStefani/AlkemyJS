236acb04a8790ca7d316154a8e6733d6a745d472
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-10-01 16:16:11 -0300

    update faltante2

--- src/api/apiLogin.js
+++ src/api/apiLogin.js
@@ -4,21 +4,11 @@ const mainURL = 'http://challenge-react.alkemy.org/';
 
 
 const apiLogin = (() => {
-    const getUserLogin = name => new Promise((resolve, reject) => {
-    axios
-      .get(
-        `${
-          mainURL + token
-        }/search/${name}`,
-      )
-      .then(data => {
-        resolve(data.data.results);
-        reject(new Error('Error en la busqueda'));
-      });
+    
   });
 
   return {
-    getHeroByName,
+    
     
   };
 })();
